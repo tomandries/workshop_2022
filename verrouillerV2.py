908a65ed51ae892212f0ab12d681901fce302837
tomandries <t.andries@hotmail.fr>
2022-10-11 11:14:39 +0200

    update verrouillerV2.py

--- verrouillerV2.py
+++ verrouillerV2.py
@@ -3,32 +3,32 @@ import datetime
 import time
 from win10toast_click import ToastNotifier
 
+# ajoute du temps à la prochaine notif
+
+
+def addBreak(time):
+    adding = datetime.timedelta(minutes=time)
+    return datetime.datetime.now() + adding
+
+
+def willLock():
+    global goLock
+    goLock = False
+    print("L'ordinateur ne se verrrouillera pas")
+
 
 # variables & objets
 toaster = ToastNotifier()
 day = datetime.datetime.now()
-pause = datetime.timedelta(minutes=1) # première notif dans 1 minutes
-timeLock = day + pause
+timeLock = addBreak(1)
 goLock = False
-
-# ajoute du temps à la prochaine notif
-def addBreak(time) :
-    adding = datetime.timedelta(minutes=time)
-    return datetime.datetime.now() + adding
-
-def willLock() :
-    global goLock
-    goLock = False
-
-toaster = ToastNotifier()
-day = datetime.datetime.now()
-pause = datetime.timedelta(minutes=1) # première notif dans 1 minutes
-timeLock = day + pause
 
 while True:
     day = datetime.datetime.now()
-    print("timeLock : " + str(timeLock))
+    print("heure actuelle : \t\t" + str(day))
+    print("heure de notificaiton : \t" + str(timeLock))
     if ((day.hour == timeLock.hour) and (day.minute == timeLock.minute)):
+        #goLock = True
         # notification
         toaster.show_toast(
             "Bien-être",  # titre
@@ -38,24 +38,10 @@ while True:
             threaded=True,  # True = run other code in parallel; False = code execution will wait till notification disappears
             callback_on_click=willLock  # click notification to run function
         )
-    timeLock = addBreak(1)
-    print("timeLock : " + str(timeLock))
-    print("goLock : " + str(goLock))
-    time.sleep(30) # pause de 30 secondes
-    if (goLock == True) :
-        ctypes.windll.user32.LockWorkStation()
-    goLock = True
-    time.sleep(10)
-
-
-
-"""
-1. Déclaration des variables
-2. TEST : verrouiller dans 5 minutes
-3. notification demande ajout 15 minutes
-4. ajouter 15 minutes
-5. ou verrouiller dans 1 minutes
-
-
-
-"""
+        timeLock = addBreak(1)
+        time.sleep(30)  # pause de 30 secondes
+        if (goLock == True):
+            print("L'ordinateur va se verrouiller")
+            time.sleep(30)
+            #ctypes.windll.user32.LockWorkStation()
+    time.sleep(20)
