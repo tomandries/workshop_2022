fdc779e2079baf142df4c1c0cb8c84aaa800558d
PierreAbr <60173098+Pierre47383@users.noreply.github.com>
2022-10-11 09:55:46 +0200

    Update pause.py
    Notification décalé le temps de la réunion

--- pause.py
+++ pause.py
@@ -1,62 +1,65 @@
+d = 1
+
 import win32com.client
 import datetime
 import time
 from plyer import notification
 
-# ouverture du calendrier Outlook
-outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-calender = outlook.GetDefaultFolder(9)  # "9" est le calendrier Outlook
-
-# affichage de la date
-day = datetime.datetime.now()
-print("Aujourd'hui, nous sommes le " + str(day.day) +
-      "/" + str(day.month) + "/" + str(day.year))
-print("Il est " + str(day.hour) + "h" + str(day.minute))
-
-# extraction du planing du jour même
-start_date = datetime.date(day.year, day.month, day.day)  # année, mois, jour
-end_date = datetime.date(day.year, day.month, day.day+1)  # année, mois, jour
-
-items = calender.Items  # récupération des rdv
-select_items = []  # liste des rdv du jour
-
-pause = datetime.timedelta(minutes=1)
-future_day = day + pause
-
-
-print("dans 1 minute(s) il sera : " +
-      str(future_day.hour) + "h" + str(future_day.minute))
-
-
-print("heure actuelle : " + str(day))
-print("heure notif : " + str(future_day))
-
-etatNotif = False
-while etatNotif == False:
+def inReu():
     day = datetime.datetime.now()
-    if ((day.hour == future_day.hour) and (day.minute == future_day.minute)):
-        # notification
-        notification.notify(
-            title="Pause",
-            message="Il est l'heure de faire une pause",
-            timeout=10,
-            app_icon="clock.ico"
-        )
-        etatNotif = True
+    # ouverture du calendrier Outlook
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+    calender = outlook.GetDefaultFolder(9)  # "9" est le calendrier Outlook
 
+    # extraction du planing du jour même
+    start_date = datetime.date(day.year, day.month, day.day)  # année, mois, jour
+    end_date = datetime.date(day.year, day.month, day.day)  # année, mois, jour
 
-"""
-for item in items:
-    if start_date <= item.start.date() <= end_date:
-        select_items.append(item)
+    items = calender.Items  # récupération des rdv
+    select_items = []  # liste des rdv du jour
 
-# affichage des détails des rdv
-for select_item in select_items:
-    print("matière:", select_item.subject)
-    print("endroit:", select_item.location)
-    print("Heure de début:", select_item.start)
-    print("Heure de fin:", select_item.end)
-    print("Texte:", select_item.body)
-    print("----")
+    for item in items:
+        if start_date <= item.start.date() <= end_date:
+            select_items.append(item)
 
-"""
+    # affichage des détails des rdv
+    for select_item in select_items:
+        if (select_item.start.hour * 100 + select_item.start.minute <= day.hour*100 + day.minute <= select_item.end.hour * 100 + select_item.end.minute):
+            return True
+    return False
+
+while True:
+
+    # affichage de la date
+    day = datetime.datetime.now()
+
+    pause = datetime.timedelta(minutes=d)
+    future_day = day + pause
+
+    print("Prochaine alerte à : " +
+        str(future_day.hour) + "h" + str(future_day.minute))
+
+    print("heure actuelle : " + str(day))
+
+    etatNotif = False
+    while etatNotif == False:
+        day = datetime.datetime.now()
+        if (inReu() == False):
+            """
+            print('futur day : ' + str(future_day.hour * 100 + future_day.minute))
+            print('now day : ' + str(day.hour * 100 + day.minute))
+            """
+            if (future_day.hour * 100 + future_day.minute <= day.hour * 100 + day.minute):
+                notification.notify(
+                    title="Pause",
+                    message="Il est temps de faire une pause",
+                    timeout=30,
+                    app_icon="clock.ico"
+                )
+                etatNotif = True
+            else:
+                time.sleep(5)
+                print('Working hard...')
+        else:
+            time.sleep(5)
+            print('In meeting...')
