692c7689baea3cea2eae540ff1c064a7ca870339
tomandries <t.andries@hotmail.fr>
2022-10-12 14:34:22 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# commande à taper en ligne de commande après la sauvegarde de ce fichier:
+# python setup.py build
+
+from cx_Freeze import setup, Executable
+  
+executables = [
+        Executable(script = "interface.py", icon = "logo.ico")
+]
+  
+buildOptions = dict( 
+        includes = ["os","tkinter", "tkinter.filedialog", "smtplib", "email.mime.multipart", "email.mime.text", "dotenv", "threading", "turtle", "win32com.client",
+         "datetime", "time", "plyer", "ctypes", "win10toast_click", "screen_brightness_control", "subprocess"],
+        include_files = ["logo.ico", "clock.ico", "dos.ico", "oeilPleure.ico", "setirer.ico", "bien-etre.png", "testfinal.py"]
+)
+  
+setup(
+    name = "SmileTime ☺",
+    version = "1.0",
+    description = "Application de bien-être au travail",
+    author = "Groupe 1",
+    options = dict(build_exe = buildOptions),
+    executables = executables
+)
